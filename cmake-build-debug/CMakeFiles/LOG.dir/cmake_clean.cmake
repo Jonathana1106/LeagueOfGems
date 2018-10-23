1bce41aa2c935aa7b7297ca2f08ed35d81bcda3b
Jonathana1196 <jonathana1196@gmail.com>
2018-10-23 17:40:07 -0600

    Creacion del proyecto

---
+++ cmake-build-debug/CMakeFiles/LOG.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LOG.dir/main.cpp.o"
+  "LOG.pdb"
+  "LOG"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LOG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
